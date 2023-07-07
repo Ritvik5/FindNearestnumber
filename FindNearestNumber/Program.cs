67fa27cee27870f766f6b408358911d7999d8857
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-07 18:48:51 +0530

    [Ritvik]Add: Welcome Message

---
+++ FindNearestNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindNearestNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Neared Number Program");
+        }
+    }
+}
